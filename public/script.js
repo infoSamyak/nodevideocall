5134d9bf3097164eac5dfb1258cad5e7532265a2
Samyak.Agrawal <samyak.agrawal@IB-SEZ-PC-241-Samyak.local>
2023-02-17 14:09:30 +0530

    changed host

--- public/script.js
+++ public/script.js
@@ -27,15 +27,6 @@ var peer = new Peer({
 	path: "/peerjs",
 	config: {
 		iceServers: [
-			{url: "stun:stun01.sipphone.com"},
-			{url: "stun:stun.ekiga.net"},
-			{url: "stun:stunserver.org"},
-			{url: "stun:stun.softjoys.com"},
-			{url: "stun:stun.voiparound.com"},
-			{url: "stun:stun.voipbuster.com"},
-			{url: "stun:stun.voipstunt.com"},
-			{url: "stun:stun.voxgratia.org"},
-			{url: "stun:stun.xten.com"},
 			{
 				url: "turn:192.158.29.39:3478?transport=udp",
 				credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
